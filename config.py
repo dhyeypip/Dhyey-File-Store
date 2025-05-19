72cda1c666638514f1b7a20efac841d885d7cb00
dhyeypip <pipatel1242@gmail.com>
2025-05-19 11:15:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ PORT = environ.get("PORT", "8080")
 CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://dhyeyfileclone:Dhyeyp@8128@clonefilestore.obkfptz.mongodb.net/?retryWrites=true&w=majority&appName=clonefilestore")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://dhyeyfileclone:Dhyeyp8128@clonefilestore.obkfptz.mongodb.net/?retryWrites=true&w=majority&appName=clonefilestore")
 CDB_NAME = environ.get("CDB_NAME", "clonefilestore")
 
 # Database Information
