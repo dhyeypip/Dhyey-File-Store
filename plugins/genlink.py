22aa6db28b8a1d10c66b00bb6ab6ffecab0c38d2
dhyeypip <pipatel1242@gmail.com>
2025-05-19 19:36:40 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -88,7 +88,7 @@ async def gen_link_batch(bot, message):
         return await message.reply("Use correct format.\nExample /batch https://t.me/vj_botz/10 https://t.me/vj_botz/20.")
     links = message.text.strip().split(" ")
     if len(links) != 3:
-        return await message.reply("Use correct format.\nExample /batch https://t.me/vj_botz/10 https://t.me/vj_botz/20.")
+        return await message.reply("Use correct format.\nExample /batch https://t.me/commonthread/10 https://t.me/commonthread/20.")
     cmd, first, last = links
     regex = re.compile("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
     match = regex.match(first)
